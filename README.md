270a70f01e822f05c7173bc0ce0418686bc35ffe
Brodski <cbrodski@gmail.com>
2023-04-04 02:08:35 -0600

    updated baby

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-pip install webdriver-manager  
-pip install beautifulsoup4  
-pip install yt_dlp  
-pip install selenium  
-pip install Flask  
+NEED PYTHON 3.10 B/C OF AI-WHISPER
+
+Start:
+$ venv\Scripts\activate
+$ deactivate
+$ python .\scrape.py
+$ python .\whisperGogogo.py
+
+
+- Apparently "*python -m* pip install -r requirements.txt" is the new meta
+- (python -m pip executes pip using the Python interpreter you specified as python. So /usr/bin/python3.7 -m pip means you are executing pip for your interpreter located at /usr/bin/python3.7.)
+
+(getting started)
+$ pip install virtualenv
+$ python -m virtualenv venv
+$ .\venv\Scripts\activate
+$ python -m pip install webdriver-manager  
+$ python -m pip install beautifulsoup4  
+$ python -m pip install yt_dlp  
+$ python -m pip install selenium  
+$ python -m pip install Flask  
+$ python -m pip install -U openai-whisper
+$ choco install ffmpeg
+
+(recall)
+pip install -r requirements.txt
+pip freeze > requirements.txt
+python -m virtualenv venv
--- whisperGogogo.py
+++ whisperGogogo.py
@@ -1,10 +1,12 @@
 import whisper
 import whisper.utils
+from whisper.utils import get_writer
+
 import os 
 # from whisper import cli
 
 model = whisper.load_model("base")
-result = model.transcribe("BarbaraWalters.mp3")
+result = model.transcribe("BarbaraWalters.mp3",  fp16=False)
 
 # model.utils.write_vtt("BarbaraWalters.mp3", "BarbaraWalters.vtt")
 # whisper.utils.write_vtt("BarbaraWalters.mp3", "BarbaraWalters.vtt")
@@ -17,18 +19,38 @@ print ("output_dir: ", output_dir )
 print ("audio_path: ", audio_path)
 print ("audio_basename: ", audio_basename)
 
+
+# Save as an SRT file
+srt_writer = get_writer("srt", output_dir)
+srt_writer(result, audio_basename + ".srt")
+
+
+# Save as a VTT file
+vtt_writer = get_writer("vtt", output_dir)
+vtt_writer(result, audio_basename + ".vtt")
+
+
+
 # from whisper.utils import write_srt
 # save VTT
-with open(os.path.join(output_dir, audio_basename + ".vtt"), "w", encoding="utf-8") as vtt:
-    whisper.utils.write_vtt(result["segments"], file=vtt)
+# with open(os.path.join(output_dir, audio_basename + ".vtt"), "w", encoding="utf-8") as vtt:
+#     whisper.utils.WriteVTT(result["segments"])
+    # whisper.utils.write_vtt(result["segments"], file=vtt)
 
 # save SRT
-with open(os.path.join(output_dir, audio_basename + ".srt"), "w", encoding="utf-8") as srt:
-    whisper.utils.write_srt(result["segments"], file=srt)
+# with open(os.path.join(output_dir, audio_basename + ".srt"), "w", encoding="utf-8") as srt:
+#     # whisper.utils.write_srt(result["segments"], file=srt)
+#     whisper.utils.WriteSRT(result["segments"], file=srt)
 
-# save TXT
-with open(os.path.join(output_dir, audio_basename + ".txt"), "w", encoding="utf-8") as txt:
-    whisper.utils.write_txt(result["segments"], file=txt)
+# # save TXT
+# with open(os.path.join(output_dir, audio_basename + ".txt"), "w", encoding="utf-8") as txt:
+#     # whisper.utils.write_txt(result["segments"], file=txt)
+#     whisper.utils.WriteTXT(result["segments"], file=txt)
+
+# # save JSON
+# with open(os.path.join(output_dir, audio_basename + ".json"), "w", encoding="utf-8") as json:
+#     # whisper.utils.write_txt(result["segments"], file=txt)
+#     whisper.utils.WriteJSON(result["segments"], file=json)
 
 # whisper.utils.write_vtt(result["segments"], file="BarbaraWalters.vtt")
 print(result["text"])

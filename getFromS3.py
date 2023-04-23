f5ecd71b902f1e636d08978ae5388e249b46176f
brodski <cbrodski@gmail.com>
2023-04-22 23:49:42 -0600

    pip freeze update

--- getFromS3.py
+++ getFromS3.py
@@ -16,3 +16,33 @@ with open('s3333.mp3', 'wb') as f:
 
 run_time = time.time() - start_time
 print(run_time)
+
+# Git
+# sudo yum update
+# sudo dnf install git-all
+#
+# Python
+# sudo yum groupinstall "Development Tools" -y
+# sudo yum install openssl-devel libffi-devel bzip2-devel -y
+# sudo curl -O https://www.python.org/ftp/python/3.10.10/Python-3.10.10.tgz
+# sudo tar -xzf Python-3.10.10.tgz
+# cd Python-3.10.10/
+# sudo  ./configure --enable-optimizations --with-ensurepip=install
+# sudo make altinstall
+# sudo ln -s /usr/local/bin/python3.10 /usr/local/bin/python
+#
+# Pip 
+# ????????????????????????????
+# cd /home/helloMe/
+# sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# sudo python get-pip.py==22.3.1
+# sudo python get-pip.py pip==23.0.1
+# pip --version
+#
+# Proj
+# sudo git clone https://github.com/Brodski/scraper-dl-vids.git
+# sudo python -m pip install virtualenv
+# sudo python -m virtualenv venv
+# pip install -r requirements.txt
+# 
+# sudo pip install requests

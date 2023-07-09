3f839ed97e3ff4923049ee05a5a0e5122b877523
brodski <cbrodski@gmail.com>
2023-07-09 06:12:30 -0600

    updates on ssscrapey, slight polishing and is_debug condition

--- audio2Text/getFromS3.py
+++ audio2Text/getFromS3.py
@@ -12,30 +12,26 @@ from Todo_model import Todo
 from typing import List
 import env_app as env_varz
 
+
+# returns -> [
+#   {
+#     "channel": "lolgeranimo",
+#     "id": "1853780899",
+#     "title": "Some_Jungle_then_Adc_Academy_later-v1853780899.mp3",
+#     "link_s3": "https://my-bucket-bigger-stronger-faster-richer-than-your-sad-bucket.s3.amazonaws.com/channels/vod-audio/lolgeranimo/1853780899/Some_Jungle_then_Adc_Academy_later-v1853780899.mp3"
+#   },
+# ]
 def _getCompletedJsonS3():
     s3 = boto3.client('s3')
-    print (env_varz.S3_COMPLETED_AUDIO_UPLOADED)
-    print (env_varz.S3_COMPLETED_AUDIO_UPLOADED)
-    print (env_varz.S3_COMPLETED_AUDIO_UPLOADED)
-
     try:
       resAudio = s3.get_object(Bucket=env_varz.BUCKET_NAME, Key=env_varz.S3_COMPLETED_TODO_AUDIO)
       todo_list_pre = json.loads(resAudio["Body"].read().decode("utf-8")) if resAudio != None else {}
-      # returns -> [
-      #   {
-      #     "channel": "lolgeranimo",
-      #     "id": "1853780899",
-      #     "title": "Some_Jungle_then_Adc_Academy_later-v1853780899.mp3",
-      #     "link_s3": "https://my-bucket-bigger-stronger-faster-richer-than-your-sad-bucket.s3.amazonaws.com/channels/vod-audio/lolgeranimo/1853780899/Some_Jungle_then_Adc_Academy_later-v1853780899.mp3"
-      #   },
-      # ]
     except:
       print("error: completed-audio json file does not exist. There is no audio to download?")
       return
     todo_list = []
     for todo in todo_list_pre:
        print(todo)
-      #  print(todo.__dict__)
        todo = Todo(channel=todo.get("channel"), id=todo.get("id"), title=todo.get("title"), link_s3=todo.get("link_s3") )
        todo_list.append(todo)
     return todo_list
@@ -51,15 +47,11 @@ def uploadCaptionsToS3(filename, todo: Todo):
     file_abs = os.path.abspath(env_varz.A2T_ASSETS_CAPTIONS + filename)
     # filename is encoded.
     filename = urllib.parse.unquote(filename)
-    print("decoded")
-    print(filename)
-    print('dump todo')
-    print('dump todo')
-    print('dump todo')
+    print("filename: " + str(filename))
+    print("todo:")
     print(json.dumps(todo, default=lambda o: o.__dict__, indent=4))
     s3CapFileKey = env_varz.S3_CAPTIONS_KEYBASE + todo.channel + "/" + todo.id + "/" + filename
 
-    
     print("channel: " + todo.channel)  
     print("vod_id: " + todo.id) 
     print("filename: " + filename) 
@@ -81,158 +73,75 @@ def get_language_code(full_language_name):
         return None
 
 
-def downloadAudio(todo_list: List[Todo]):
+def downloadAudio(todo: Todo):
   print("######################################")
   print("             downloadAudio            ")
   print("######################################")
-  for todo in todo_list:
-    print("-----------------------------------")
-    print("link_path_todo: " + str(todo))
-    audio_url = todo.link_s3
-    print("")
-    print("")
-    print("")
-    print("audio_url before=" + str(audio_url))
+  print("link_path_todo: " + str(todo))
+  audio_url = todo.link_s3
+  print("")
+  print("")
+  print("")
+  print("audio_url before=" + str(audio_url))
 
-    audio_name = os.path.basename(audio_url) # A trick to get the file name. eg) filename = "Calculated-v5057810.mp3"
-    audio_name_encode = urllib.parse.quote(audio_name)
-    meta_url = audio_url.replace(audio_name, "metadata.json")
-    audio_url = audio_url.replace(audio_name, audio_name_encode)    
-    relative_filename = env_varz.A2T_ASSETS_AUDIO +  audio_name_encode
+  audio_name = os.path.basename(audio_url) # A trick to get the file name. eg) filename = "Calculated-v5057810.mp3"
+  audio_name_encode = urllib.parse.quote(audio_name)
+  meta_url = audio_url.replace(audio_name, "metadata.json")
+  audio_url = audio_url.replace(audio_name, audio_name_encode)    
+  relative_filename = env_varz.A2T_ASSETS_AUDIO +  audio_name_encode
 
-    print("audio_url=" + str(audio_url))
-    print("audio_name=" + str(audio_name))
-    print("audio_name_encode=" + str(audio_name_encode))
-    print("meta_url=" + str(meta_url))
-    print("relative_filename=" + str(relative_filename))    
+  print("audio_url=" + str(audio_url))
+  print("audio_name=" + str(audio_name))
+  print("audio_name_encode=" + str(audio_name_encode))
+  print("meta_url=" + str(meta_url))
+  print("relative_filename=" + str(relative_filename))    
 
-    # Audio file download locally
-    local_filename  = urllib.request.urlretrieve(audio_url, relative_filename)
+  # Audio file download locally
+  local_filename  = urllib.request.urlretrieve(audio_url, relative_filename) # audio_url = Calculated-v123123.ogg
+  print("local_filename")
+  print(local_filename)
+  print(local_filename)
+  print(local_filename)
+  print(local_filename)
+  print(local_filename)
+  # Metadata.json get in memory
+  response_meta = urllib.request.urlopen(meta_url)
+  data = response_meta.read()
+  metadata_file_s3 = json.loads(data)
 
-    # Metadata.json get in memory
-    response_meta = urllib.request.urlopen(meta_url)
+  return audio_name_encode, metadata_file_s3
 
-    data = response_meta.read()
-    json_data = json.loads(data)
+def cleanUpFiles(filename):
+   
+  file_abs = os.path.abspath(env_varz.A2T_ASSETS_AUDIO + filename)
+  print("cleanUpFiles - file_abs= " + file_abs)
+  print("cleanUpFiles - file_abs= " + file_abs)
+  print("cleanUpFiles - file_abs= " + file_abs)
+  if os.getenv("ENV") != "local":
+     print("DELETING IT!!!!!!!")
+     os.remove(file_abs)
+  print()
+  # os.remove(file_abs)
 
-    lang_code = get_language_code(json_data["language"])
+def doWhisperStuff(audio_name_encode, metadata_file_s3):
+    lang_code = get_language_code(metadata_file_s3["language"])
     print()
     print()
-    print("      json_data")
-    print("    " + json_data["channel"])
-    print("    " + json_data["link"])
-    print("    " + json_data["language"])
+    print("      metadata_file_s3")
+    print("    " + metadata_file_s3["channel"])
+    print("    " + metadata_file_s3["link"])
+    print("    " + metadata_file_s3["language"])
     print("    " + str(lang_code))
     print("    " + "audio_name_encode=" + audio_name_encode)
-    # print (json_data["url"])
-    print("local_filename")
-    print(local_filename)
     saved_caption_files = gogoWhisperFAST.run(model_size="base", lang_code=lang_code, filename=audio_name_encode)
 
     for filename in saved_caption_files: # [vodx.json, vodx.vtt]
-      # uploadCaptionsToS3(filename, json_data["channel"], json_data["link"].split("/")[-1])
       s3_file_location = uploadCaptionsToS3(filename, todo)
-    return s3_file_location
+    cleanUpFiles(audio_name_encode)
+    return True
     # if s3_file_location:
     #    updateCompletedCaptionsS3(s3_file_location, todo)
-    
-# # THIS IS PRACTICALLY COPY PASTED FROM mainController.js
-# def updateCompletedCaptionsS3(s3_file_location, todo: Todo):
-#     allOfIt = _getAllCompletedJsonSuperS3__BETTER()
-#     print("allOfIt")
-#     print(allOfIt)
-#     print()
-#     print()
-#     print()
-#     print(json.dumps(allOfIt.__dict__, default=lambda o: o.__dict__))
-#     if allOfIt is None or len(allOfIt)==0:
-#         raise Exception("Something is wrong with '_getCompletedAudioJsonSuperS3' audio json file")
-#     captions_ext = ['.json', '.vtt']
-#     missing_captions_list = []
 
-#     for k_chn, v_id_files in allOfIt.items():
-#         print(k_chn)
-#         for id, files in v_id_files.items():
-#             # print ()
-#             # print ("    " + str(id))
-#             # print ("    " + str(files))
-#             # hasAudio = False
-#             hasCaptions = False
-#             for file in files:
-#                 if file == "metadata.json":
-#                     continue
-#                 if file[-5:] not in captions_ext and file[-4:] not in captions_ext:
-#                     hasAudio = True
-#                     vod_title = file
-#                     continue
-#                 if file[-5:] in captions_ext or file[-4:] in captions_ext:
-#                     hasCaptions = True
-#             if hasAudio and not hasCaptions:
-#                 print("MISSING CAPTIONS for: " + k_chn + " " + id)
-#                 print()
-#                 vod = Vod(channel=k_chn, id=id, title=urllib.parse.unquote(vod_title))
-#                 vod_encode = urllib.parse.quote(vod.title)
-#                 vod_path = env_varz.S3_CAPTIONS_KEYBASE + vod.channel + "/" + vod.id + "/" + vod_encode
-#                 vod.link_s3 =  env_varz.BUCKET_DOMAIN + "/" + vod_path
-
-#                 missing_captions_list.append(vod)
-#     s3 = boto3.client('s3')
-#     s3.put_object(Body=json.dumps(missing_captions_list, default=lambda o: o.__dict__), Bucket=env_varz.BUCKET_NAME, Key=env_varz.S3_COMPLETED_TODO_AUDIO)
-#     print ("MISSSING!")
-#     print ("MISSSING!")
-#     print ("MISSSING!")
-#     print ("MISSSING!")
-#     print (missing_captions_list)
-#     print (json.dumps(missing_captions_list, default=lambda o: o.__dict__, indent=4))
-#     if isDebug:
-#         return json.loads(json.dumps(missing_captions_list, default=lambda o: o.__dict__)) 
-#     return missing_captions_list
-
-
-
-
-
-# COPY AND PASTED CODE FROM mainController (b/c they are 2 differents servers)
-# COPY AND PASTED CODE FROM mainController (b/c they are 2 differents servers)
-def _getAllCompletedJsonSuperS3__BETTER(): # -> mocks/completedJsonSuperS3.py
-    s3 = boto3.client('s3')
-    objects = s3.list_objects_v2(Bucket=env_varz.BUCKET_NAME, Prefix=env_varz.S3_CAPTIONS_KEYBASE)['Contents']
-    sorted_objects = sorted(objects, key=lambda obj: obj['Key'])
-    print("----- _getCompletedAudioJsonSuperS3 ---- ")
-    
-    allOfIt = {}
-    for obj in sorted_objects:
-        filename = obj['Key'].split("/")[4:][0]
-        vod_id = obj['Key'].split("/")[3:4][0]
-        channel = obj['Key'].split("/")[2:3][0]
-        print("@@@@@@@@@@@@@@@@@@@@@")
-        print("Key= " + f"{obj['Key']}")
-        print("channel: " +  (channel))     
-        print("vod_id: " +  (vod_id))
-        print("filename: " + (filename))
-        # 1. obj[key] = channels/vod-audio/lolgeranimo/5057810/Calculated-v5057810.mp3
-        # 2. temp = lolgeranimo/5057810/Calculated-v5057810.mp3
-        # 3. channel, vod_i, vod_title = [ lolgeranimo, 5057810, "Calculated-v5057810.mp3" ] 
-        temp = str(obj['Key']).split(env_varz.S3_CAPTIONS_KEYBASE, 1)[1]   # 2
-        # channel, vod_id, vod_title = temp.split("/", 2)[:3] # 3 
-        if allOfIt.get(channel):
-            if allOfIt.get(channel).get(vod_id): # if vod_id for channel exists
-                allOfIt.get(channel).get(vod_id).append(filename)
-            else: # else create a list that has all filenames
-                allOfIt.get(channel)[vod_id] = [filename]
-        else:
-            vod_dict = { vod_id: [filename] }
-            allOfIt[channel] = vod_dict
-    print ()
-    print ("(_getAllCompletedJsonSuperS3__BETTER) allOfIt=")
-    print (json.dumps(allOfIt, default=lambda o: o.__dict__, indent=4))
-    print ()
-    # for key, value in allOfIt.items():
-    #     print(key + ": " + str(value))
-    return allOfIt
-
-
-    
 # todo_list
 # {
 #     "channel": "lolgeranimo",
@@ -245,35 +154,33 @@ def _getAllCompletedJsonSuperS3__BETTER(): # -> mocks/completedJsonSuperS3.py
 #     "id": "1856310873",
 #     "title": "How_to_Climb_on_Adc_So_washed_up_i_m_clean_-_hellofresh-v1856310873.mp3",
 #     "link_s3": "https://my-bucket-bigger-stronger-faster-richer-than-your-sad-bucket.s3.amazonaws.com/channels/vod-audio/lolgeranimo/1856310873/How_to_Climb_on_Adc_So_washed_up_i_m_clean_-_hellofresh-v1856310873.mp3"
-# }
-
-  # "lolgeranimo": {
-  #   "28138895": [
-  #     "The Geraniproject! I Love You Guys!!!-v28138895.json",
-  #     "The Geraniproject! I Love You Guys!!!-v28138895.mp3",
-  #     "The Geraniproject! I Love You Guys!!!-v28138895.vtt",
-  #     "metadata.json"
-  #   ],
+# } 
 if __name__ == '__main__':
-  super_start = time.time()
-  print('hello')
-  todo_list = _getCompletedJsonS3() 
-  print("##########")
-  print("todo_listzzzz")
-  print(todo_list)
-  todo = Todo(channel="lolgeranimo", id="28138895", title="The Geraniproject! I Love You Guys!!!-v28138895", link_s3="https://my-bucket-bigger-stronger-faster-richer-than-your-sad-bucket.s3.amazonaws.com/channels/vod-audio/lolgeranimo/28138895/The_Geraniproject_I_Love_You_Guys-v28138895.mp3" )
-  todo_list = [todo]
-  print()
-  print('asdasda')
-  for todo in todo_list:
-    print(json.dumps(todo, default=lambda o:o.__dict__, indent=4))
-  print()
-  s3_file_location = downloadAudio(todo_list)
-  end = time.time() - super_start
-  some_obj = {
-     "time": end,
-     "s3_file_location": s3_file_location
-  }
-  s3 = boto3.client('s3')
-  s3.put_object(Body=json.dumps(some_obj, default=lambda o: o.__dict__), Bucket=env_varz.BUCKET_NAME, Key=env_varz.S3_COMPLETED_CAPTIONS_DONE)
+    isDebug = env_varz.IS_DEBUG
+    super_start = time.time()
+    print('Start')
+    print(os.getenv("ENV"))
+    print(os.getenv("ENV"))
+    todo_list = _getCompletedJsonS3() 
+    print("##########")
+    print()
+    if isDebug and os.getenv("ENV") == "local":
+      todo = Todo(channel="lolgeranimo", id="28138895", title="The Geraniproject! I Love You Guys!!!-v28138895", link_s3="https://my-bucket-bigger-stronger-faster-richer-than-your-sad-bucket.s3.amazonaws.com/channels/vod-audio/lolgeranimo/28138895/The_Geraniproject_I_Love_You_Guys-v28138895.mp3" )
+      todo_list = [todo]
+
+    for todo in todo_list:
+      print("---")
+      print(json.dumps(todo, default=lambda o:o.__dict__, indent=4))
+      # local_filename, metadata_file_s3 = downloadAudio(todo_list)
+      audio_name_encode, metadata_file_s3 = downloadAudio(todo)
+      doWhisperStuff(audio_name_encode, metadata_file_s3)
+    print("done!!")
+    
+    # end = time.time() - super_start
+    # some_obj = {
+    #     "time": end,
+    #     "s3_file_location": s3_file_location
+    # }
+    # s3 = boto3.client('s3')
+    # s3.put_object(Body=json.dumps(some_obj, default=lambda o: o.__dict__), Bucket=env_varz.BUCKET_NAME, Key=env_varz.S3_COMPLETED_CAPTIONS_DONE)
 
--- audio2Text/gogoWhisperFAST.py
+++ audio2Text/gogoWhisperFAST.py
@@ -11,7 +11,6 @@
 import faster_whisper
 # import whisper.utils
 from whisper.utils import get_writer
-import boto3
 
 import os 
 import time
@@ -21,13 +20,7 @@ import argparse
 from pathlib import Path
 
 import env_app as env_varz
-# import requests
 
-# Should be env vairable for local or micro
-# env_varz.A2T_ASSETS_AUDIO = "./assets/audio/"
-# env_varz.A2T_ASSETS_CAPTIONS = "./assets/output/"
-MAIN_DIR = r'/home/ssm-user/scraper-dl-vids'
-MAIN_DIR = r'C:/Users/BrodskiTheGreat/Desktop/desktop/Code/scraper-dl-vids'
 FILE_EXTENSIONS_TO_SAVE = ["json", "vtt"]
 
 def main():
@@ -37,8 +30,8 @@ def main():
     # filename = "Bootcamp to Challenger - Gaming-v1767827635.f_Audio_Only.mp4"
     # filename = "Bootcamp to Challenger - Gaming-v1767827635.f_Audio_Only.mp3"
     # filename = "Bootcamp to Challenger ｜-v1747933567.f_Audio_Only-wtf.mp3"
-    filename = "OPENASSISTANT+TAKES+ON+CHATGPT.mp3"
     # filename = "Adc+Academy+-+Informative+Adc+Stream+-+GrandMaster+today？+[v1792628012].mp3"
+    filename = "OPENASSISTANT+TAKES+ON+CHATGPT.mp3"
     filename = "BarbaraWalters.mp3"
     
     #model_size = "large-v2"
@@ -53,22 +46,14 @@ def main():
     parser.add_argument('-f', "--file", help="file to use")
     parser.add_argument('-m', "--model", help="model: tiny, base, small, med, large-v2, tiny.en, base.en, ... https://github.com/openai/whisper#available-models-and-languages  ||  https://huggingface.co/openai/whisper-large")
     args = parser.parse_args()
-
     if args.model:
         model_size = args.model
     if args.file:
         filename = args.file
-        # filename = Path(args.file)
-        
-    print ("sys.argv")
-    print (sys.argv)
-    print ("args")
-    print ("args")
-    print ("args")
-    print (args)
-    print ("args.file")
-    print (args.file)
-    print (args.file)
+    print()
+    print("sys.argv: " + str(sys.argv))
+    print("args: " + str(args))
+    print("args.file: " + str(args.file))
 
     run(model_size=model_size, filename=filename)
 
@@ -76,36 +61,25 @@ def main():
 ######################################################
 # APP
 ######################################################
-def run(*, model_size, lang_code, filename):
+def run(*, model_size, lang_code=None, filename):
     model_size = env_varz.WHSP_MODEL_SIZE
     compute_type = env_varz.WHSP_COMPUTE_TYPE
     cpu_threads = int(env_varz.WHSP_CPU_THREADS)
 
     # model = faster_whisper.WhisperModel(model_size, device="cuda", compute_type="int8", cpu_threads=8) # 4 default 
     model = faster_whisper.WhisperModel(model_size, compute_type=compute_type,  cpu_threads=cpu_threads) # 4 default
-    # audio_path = "{}/{}/{}".format(MAIN_DIR, env_varz.A2T_ASSETS_AUDIO, filename)
     audio_abs_path = os.path.abspath(env_varz.A2T_ASSETS_AUDIO + '/' + filename)
     audio_basename = os.path.basename(env_varz.A2T_ASSETS_AUDIO + '/' + filename)
-    
-    model_size = env_varz.WHSP_MODEL_SIZE
-    compute_type = env_varz.WHSP_COMPUTE_TYPE
-    cpu_threads = int(env_varz.WHSP_CPU_THREADS)
 
-
-    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-    # print(audio_path)
-    # print(audio_path)
-    print(audio_abs_path)
-    print(audio_abs_path)
-    # exit()
     start_time = time.time()
 
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print("audio_abs_path: " + audio_abs_path)
     print("torch.cuda.is_available(): " + str(torch.cuda.is_available()))
-    print("start!")
-    # segments, info = model.transcribe(audio_abs_path, language="en", condition_on_previous_text=False, beam_size=2, best_of=2)
-    # segments, info = model.transcribe(audio_abs_path, language="en", condition_on_previous_text=False, vad_filter=True)
     # segments, info = model.transcribe(audio_abs_path, language="en")
-    segments, info = model.transcribe(audio_abs_path, language=lang_code, condition_on_previous_text=False, vad_filter=True, beam_size=2, best_of=2)
+    # segments, info = model.transcribe(audio_abs_path, language="en", condition_on_previous_text=False, vad_filter=True)
+    segments, info = model.transcribe(audio_abs_path, language=lang_code, condition_on_previous_text=False, vad_filter=True, beam_size=2, best_of=2) # vad_filter = something to prevents bugs. long loops being stuck
+    
 
     print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
@@ -122,10 +96,26 @@ def run(*, model_size, lang_code, filename):
         })
 
 
-    # file_extension = "json"
-    # srt_writer = get_writer(file_extension, env_varz.A2T_ASSETS_CAPTIONS)
-    # srt_writer(result, audio_basename + file_extension)
     saved_caption_files = writeCaptionsLocally(result, audio_basename)
+    # removeAudio(audio_basename)
+    print( "audio_basename: ")
+    print(audio_basename)
+    print(audio_basename)
+    print(audio_basename)
+    print(audio_basename)
+    print(audio_basename)
+    print(audio_basename)
+    print(audio_basename)
+    print(audio_basename)
+    print(audio_basename)
+    print(audio_basename)
+    print(audio_basename)
+    # HERE
+    # HERE
+    # HERE
+    # HERE
+    # HERE
+    # HERE
     end_time = time.time() - start_time
 
     print("========================================")
@@ -136,8 +126,7 @@ def run(*, model_size, lang_code, filename):
     print()
     print("Saved files: " + str(saved_caption_files))
     print()
-    # print("Completed srt: " +  audio_basename + ".srt")
-    # print("Completed: " + audio_path)
+
     return saved_caption_files
 
 def writeCaptionsLocally(result, audio_basename):
@@ -147,9 +136,10 @@ def writeCaptionsLocally(result, audio_basename):
     for ext in file_extensions:
         srt_writer = get_writer(ext, env_varz.A2T_ASSETS_CAPTIONS)
         srt_writer(result, audio_basename + ext)
-        print("audio_basename")
-        print("audio_basename")
-        print(audio_basename)
+        print("audio_basename: " + audio_basename)
+        print("audio_basename: " + audio_basename)
+        print("audio_basename: " + audio_basename)
+        print("audio_basename: " + audio_basename)
 
         filename, file_extension = os.path.splitext(audio_basename) # [Calculated-v5057810, .mp3]
         caption_file = filename + '.' + ext

e5bfd8bbcd4aaa9b82ca616654abe7ad4b01fe27
brodski <cbrodski@gmail.com>
2024-01-01 00:06:05 -0700

    trying to get terraform to work

--- SSScrapey-rework/kickWhisperer.py
+++ SSScrapey-rework/kickWhisperer.py
@@ -1,4 +1,10 @@
+print("GGGGGGGGGGOOOOOOOOOOOOOOOOING")
+print("GGGGGGGGGGOOOOOOOOOOOOOOOOING")
+print("GGGGGGGGGGOOOOOOOOOOOOOOOOING")
 import controllers.MicroTranscriber.audio2text_app as audio2text_app
 
 if __name__ == "__main__":
+    print("GGGGGGGGGGOOOOOOOOOOOOOOOOING")
+    print("GGGGGGGGGGOOOOOOOOOOOOOOOOING")
+    print("GGGGGGGGGGOOOOOOOOOOOOOOOOING")
     audio2text_app.gogo()

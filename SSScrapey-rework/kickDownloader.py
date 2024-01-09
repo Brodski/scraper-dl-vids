17c439282e61b3165657e24da5a542c40a703b81
brodski <cbrodski@gmail.com>
2024-01-09 00:24:26 -0700

    1line

--- SSScrapey-rework/kickDownloader.py
+++ SSScrapey-rework/kickDownloader.py
@@ -1,4 +1,4 @@
-import controllers.MicroDownloader.go as downloadGo
+import controllers.MicroDownloader.downloaderGo as downloadGo
 
 if __name__ == "__main__":
     x = downloadGo.goDownloadBatch(isDebug=False)

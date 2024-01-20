14740a49303273fcb8d512383adb60c0888a17bd
brodski <cbrodski@gmail.com>
2024-01-20 03:28:53 -0700

    wut

--- SSScrapey-rework/kickDownloader.py
+++ SSScrapey-rework/kickDownloader.py
@@ -1,5 +1,6 @@
 import controllers.MicroDownloader.downloaderGo as downloadGo
 
 if __name__ == "__main__":
+    print('starting go download batch...')
     x = downloadGo.goDownloadBatch(isDebug=False)
     print('Finished kickDownloader')

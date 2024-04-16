93169a0bf0ce99f289d701b5199bb19d43b67c67
brodski <cbrodski@gmail.com>
2024-04-16 00:03:59 -0600

    whis

--- insanely-fast/test_helperz/whispererInsanelyFast.py
+++ insanely-fast/test_helperz/whispererInsanelyFast.py
@@ -1,4 +1,3 @@
-
 import os
 import time
 
@@ -16,16 +15,20 @@ import faster_whisper
 import faster_whisper.utils
 from whisper.utils import get_writer
 
+# nvcc and cuda setup v2 https://www.freecodecamp.org/news/how-to-install-nvidia-cuda-toolkit-on-ubuntu/
+
 # https://github.com/Vaibhavs10/insanely-fast-whisper/issues/158 vad = bad?
 
 # filename = "Calculated-v5057810.opus"
 # audio_url = "https://my-dev-bucket-bigger-stronger-faster-richer-than-your-bucket.s3.amazonaws.com/channels/vod-audio/lolgeranimo/5057810/Calculated-v5057810.opus"
 
-# filename = "Adc academy ｜ How to climb on Adc - Grandmaster Climb --v1802413591.mp3"
+# filename = "Adc academy __ How to climb on Adc - Grandmaster Climb --v1802413591.mp3"
 filename = "The_Geraniproject_I_Love_You_Guys-v28138895.opus"
 audio_url = "https://my-dev-bucket-bigger-stronger-faster-richer-than-your-bucket.s3.us-east-1.amazonaws.com/channels/vod-audio/lolgeranimo/28138895/The_Geraniproject_I_Love_You_Guys-v28138895.opus"
 model_size_insane = "openai/whisper-medium"
+model_size_insane = "openai/whisper-large-v3"
 model_size_fast = "medium"
+model_size_fast = "large-v3"
 my_device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
@@ -38,14 +41,14 @@ def downloadAudio():
         print("File downloaded successfully!")
     else:
         print(f"Failed to download file. Status code: {response.status_code}")
-
+# pipline tutorial https://huggingface.co/docs/transformers/v4.39.3/en/pipeline_tutorial
 def goInsaneoMode():
-    pipe = pipeline(
+    pipe = pipeline( # https://huggingface.co/docs/transformers/main_classes/pipelines#transformers.pipeline
         "automatic-speech-recognition",
-        # model="openai/whisper-large-v3", # select checkpoint from https://huggingface.co/openai/whisper-large-v3#model-details
-        model="openai/whisper-medium", 
-        torch_dtype=torch.float32,
-        device=my_device, 
+        model=model_size_insane, # select checkpoint from https://huggingface.co/openai/whisper-large-v3#model-details
+#        model="openai/whisper-medium",
+        torch_dtype=torch.float16,
+        device=my_device,
         model_kwargs={"attn_implementation": "flash_attention_2"} if is_flash_attn_2_available() else {"attn_implementation": "sdpa"},
     )
     # https://github.com/Vaibhavs10/insanely-fast-whisper/issues/6
@@ -54,15 +57,16 @@ def goInsaneoMode():
         "language": 'en',
         "temperature": 0.2,
         "repetition_penalty": 3.0,
-        # "condition_on_previous_text": True,
-        "task": "transcribe"
+      #  "condition_on_previous_text": True,
+        "task": "transcribe",
     }
     start_time = time.time()
     outputs = pipe(
         filename,
         chunk_length_s=30,
         batch_size=24,
-        return_timestamps=True,
+        return_timestamps="word",
+#        return_timestamps=True,
         generate_kwargs = generate_kwargs
     )
     return outputs, start_time
@@ -72,11 +76,12 @@ def doWhisperStuff( relative_path: str):
     print(relative_path)
     file_abspath = os.path.abspath(relative_path) # if relative_path =./assets/audio/ft.-v1964894986.opus then => file_abspath = C:\Users\BrodskiTheGreat\Desktop\desktop\Code\scraper-dl-vids\SSScrapey-rework\And_you_will_know_my_name_is_the_LORD-v40792901.opus
     file_name = os.path.basename(relative_path) # And_you_will_know_my_name_is_the_LORD-v40792901.opus
-
+    end_time = None
 
     print("    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     print("    file_abspath=" + file_abspath)
     print("    torch.cuda.is_available(): " + str(torch.cuda.is_available()))
+    print("    is_flash_attn_2_available(): " + str(is_flash_attn_2_available()))
     outputs, start_timeX = goInsaneoMode()
     print("outputs")
     print(outputs)
@@ -90,16 +95,21 @@ def doWhisperStuff( relative_path: str):
             # print(chunk['text'].strip())
             prev, start_time = seconds_to_srt_time_format(prev, chunk['timestamp'][0])
             prev, end_time = seconds_to_srt_time_format(prev, chunk['timestamp'][1])
-            srt_file.write(f"{index + 1}\n")
-            srt_file.write(f"{start_time} --> {end_time}\n")
-            srt_file.write(f"{chunk['text'].strip()}\n\n")
-            print(f"{index + 1}")
-            print(f"{start_time} --> {end_time}")
+#            //srt_file.write(f"{index + 1}\n")
+            srt_file.write(f"{start_time} --> {end_time}: ")
+            srt_file.write(f"{chunk['text'].strip()}\n")
+            # print(f"{index + 1}")
+            # print(f"{start_time} --> {end_time}")
+            # print(f"{chunk['text'].strip()}\n")
+            print(f"{start_time} --> {end_time}: ")
             print(f"{chunk['text'].strip()}\n")
+        
+        end_time = time.time() - start_timeX
+        srt_file.write(f"Run time: {end_time}\n")
+        srt_file.write(f"model_size_insane: {model_size_insane}\n")
 
-    
+
     # saved_caption_files = writeCaptionsLocally(result, file_name)
-    end_time = time.time() - start_timeX
 
     print("========================================")
     print("Complete!")
@@ -146,7 +156,7 @@ def mp3FastTranscribe(filename):
     start_time = time.time()
     segments, info = model.transcribe(filename, language="en")
 
-    result = { 
+    result = {
         "segments": []
     }
 
@@ -177,7 +187,4 @@ def mp3FastTranscribe(filename):
 if __name__ == "__main__":
     relative_path = downloadAudio()
     saved_caption_files = doWhisperStuff(filename)
-
-
-
-
+#    mp3FastTranscribe(filename)

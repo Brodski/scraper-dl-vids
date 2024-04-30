1c5025065557f25396ce76498a84824f16f45525
brodski <cbrodski@gmail.com>
2024-04-30 06:05:02 -0600

    scriptz

---
+++ insanely-fast/test_helperz/install_flash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install flash-attn --no-build-isolation
+
+# exec "$@"
+
+
+pip install torch
+pip install transformers
+pip install flash-attn --no-build-isolation
---
+++ insanely-fast/test_helperz/run_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "NOT DONE!!!!!!!!!!!!"
+git pull
+echo "gonna install?"
+# cat install_flash.sh
+pip --version
+# cat /etc/os-release
+echo "---"
+env
+# sh install_flash.sh
+pip install flash-attn --no-build-isolation
+python whispererInsanelyFast.py
+echo "DONE!!!!!!!!!!!!"
+echo "DONE!!!!!!!!!!!!"
+echo "DONE!!!!!!!!!!!!"

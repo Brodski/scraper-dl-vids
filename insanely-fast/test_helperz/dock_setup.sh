5ab1c43e61449c06565760566ea0454ffbbdd60b
brodski <cbrodski@gmail.com>
2024-05-01 04:40:53 -0600

    dock update

--- insanely-fast/test_helperz/dock_setup.sh
+++ insanely-fast/test_helperz/dock_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ "$ENV" = "dev" ] || [ "$ENV" = "local" ]; then
+# if [ "$ENV" = "dev" ] || [ "$ENV" = "local" ]; then
     git fetch origin dev ;
     git checkout dev ;
     git reset --hard origin/dev ; 
     git pull origin dev --force ;
     echo "Running in development mode"
-else
-    git reset --hard origin/master ;
-    git pull origin master --force ; 
-    echo "Running in production mode"
-fi
+# else
+#     git reset --hard origin/master ;
+#     git pull origin master --force ; 
+#     echo "Running in production mode"
+# fi
 
 # exec "$@"

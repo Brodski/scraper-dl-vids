330dda3d8cab0ddd7182d873e257fc70d960e139
brodski <cbrodski@gmail.com>
2024-05-01 03:22:56 -0600

    better logging

--- insanely-fast/test_helperz/whispererInsanelyFast.py
+++ insanely-fast/test_helperz/whispererInsanelyFast.py
@@ -11,6 +11,8 @@ from transformers.utils import is_flash_attn_2_available
 
 import os
 import time
+import cloudwatch as Cloudwatch
+import os
 # import faster_whisper
 # import faster_whisper.utils
 # from whisper.utils import get_writer
@@ -45,17 +47,22 @@ model_size_fast = "medium"
 model_size_fast = "large-v3"
 my_device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
+def logger():
+    pass
+logger = Cloudwatch.log
+
+
 # "You are attempting to use Flash Attention 2.0 with a model not initialized on GPU. Make sure to move the model to GPU after initializing it on CPU with `model.to('cuda')`."
 # ^ Ignore https://github.com/Vaibhavs10/insanely-fast-whisper/issues/141
 def downloadAudio():
-    print(audio_url)
+    logger(audio_url)
     response = requests.get(audio_url)
     if response.status_code == 200:
         with open(filename, 'wb') as f:
             f.write(response.content)
-        print("File downloaded successfully!")
+        logger("File downloaded successfully!")
     else:
-        print(f"Failed to download file. Status code: {response.status_code}")
+        logger(f"Failed to download file. Status code: {response.status_code}")
 # pipline tutorial https://huggingface.co/docs/transformers/v4.39.3/en/pipeline_tutorial
 def goInsaneoMode():
     pipe = pipeline( # https://huggingface.co/docs/transformers/main_classes/pipelines#transformers.pipeline
@@ -93,37 +100,37 @@ def goInsaneoMode():
     return outputs, start_time
 
 def doWhisperStuff( relative_path: str):
-    print("Starting WhisperStuff!")
-    print(relative_path)
+    logger("Starting WhisperStuff!")
+    logger(relative_path)
     file_abspath = os.path.abspath(relative_path) # if relative_path =./assets/audio/ft.-v1964894986.opus then => file_abspath = C:\Users\BrodskiTheGreat\Desktop\desktop\Code\scraper-dl-vids\SSScrapey-rework\And_you_will_know_my_name_is_the_LORD-v40792901.opus
     file_name = os.path.basename(relative_path) # And_you_will_know_my_name_is_the_LORD-v40792901.opus
     end_time = None
 
-    print("    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-    print("    file_abspath=" + file_abspath)
-    print("    torch.cuda.is_available(): " + str(torch.cuda.is_available()))
-    print("    is_flash_attn_2_available(): " + str(is_flash_attn_2_available()))
+    logger("    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    logger("    file_abspath=" + file_abspath)
+    logger("    torch.cuda.is_available(): " + str(torch.cuda.is_available()))
+    logger("    is_flash_attn_2_available(): " + str(is_flash_attn_2_available()))
     outputs, start_timeX = goInsaneoMode()
-    print("outputs")
-    print(outputs)
+    logger("outputs")
+    logger(outputs)
     audio_file_name = os.path.splitext(os.path.basename(filename))[0]
     srt_filename = f"{audio_file_name}.srt"
     with open(srt_filename, 'w') as srt_file:
         prev = 0
         for index, chunk in enumerate(outputs['chunks']):
-            # print('---')
-            # print(chunk['timestamp'][0])
-            # print(chunk['text'].strip())
+            # logger('---')
+            # logger(chunk['timestamp'][0])
+            # logger(chunk['text'].strip())
             prev, start_time = seconds_to_srt_time_format(prev, chunk['timestamp'][0])
             prev, end_time = seconds_to_srt_time_format(prev, chunk['timestamp'][1])
 #            //srt_file.write(f"{index + 1}\n")
             srt_file.write(f"{start_time} --> {end_time}: ")
             srt_file.write(f"{chunk['text'].strip()}\n")
-            # print(f"{index + 1}")
-            # print(f"{start_time} --> {end_time}")
-            # print(f"{chunk['text'].strip()}\n")
-            print(f"{start_time} --> {end_time}: ")
-            print(f"{chunk['text'].strip()}\n")
+            # logger(f"{index + 1}")
+            # logger(f"{start_time} --> {end_time}")
+            # logger(f"{chunk['text'].strip()}\n")
+            logger(f"{start_time} --> {end_time}: ")
+            logger(f"{chunk['text'].strip()}\n")
         
         end_time = time.time() - start_timeX
         srt_file.write(f"Run time: {end_time}\n")
@@ -132,17 +139,17 @@ def doWhisperStuff( relative_path: str):
 
     # saved_caption_files = writeCaptionsLocally(result, file_name)
 
-    print("========================================")
-    print("Complete!")
-    # print(f"Detected language {info.language} with probability {str(info.language_probability)}")
-    # print()
-    print("run time =" + str(end_time))
-    # print()
-    # print("Saved files: " + str(saved_caption_files))
-    # print()
-    # print("model_size: " + model_size)
-    # print()
-    print("========================================")
+    logger("========================================")
+    logger("Complete!")
+    # logger(f"Detected language {info.language} with probability {str(info.language_probability)}")
+    # logger()
+    logger("run time =" + str(end_time))
+    # logger()
+    # logger("Saved files: " + str(saved_caption_files))
+    # logger()
+    # logger("model_size: " + model_size)
+    # logger()
+    logger("========================================")
     time.sleep(300)
     return True
 
@@ -169,8 +176,8 @@ def seconds_to_srt_time_format(prev, seconds):
 # $ ffmpeg -i .\BarbaraWaltersFAST.mp3 -filter:a "atempo=1.5" "BarbaWaltersFASTER.mp3"
 
 # def mp3FastTranscribe(filename):
-#     print("    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-#     print("    torch.cuda.is_available(): " + str(torch.cuda.is_available()))
+#     logger("    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+#     logger("    torch.cuda.is_available(): " + str(torch.cuda.is_available()))
 #     model = faster_whisper.WhisperModel(model_size_fast, compute_type="int8",  cpu_threads=16) # 4 default
 #     # audio_path = "{}/{}/{}".format(main_dir, asset_dir, filename)
 
@@ -182,7 +189,7 @@ def seconds_to_srt_time_format(prev, seconds):
 #     }
 
 #     for segment in segments:
-#         print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+#         logger("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
 #         result["segments"].append({
 #             "start" : segment.start,
 #             "end" :   segment.end,
@@ -195,11 +202,11 @@ def seconds_to_srt_time_format(prev, seconds):
 
 #     end_time = time.time() - start_time
 
-#     print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
-#     print()
-#     print("run time =" + str(end_time))
-#     print()
-#     print("Completed: " + filename)
+#     logger("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+#     logger()
+#     logger("run time =" + str(end_time))
+#     logger()
+#     logger("Completed: " + filename)
 
 
 
@@ -209,6 +216,6 @@ if __name__ == "__main__":
     relative_path = downloadAudio()
     saved_caption_files = doWhisperStuff(filename)
     time.sleep(120)
-    print("GG!")
-    print("GG!")
+    logger("GG!")
+    logger("GG!")
 #    mp3FastTranscribe(filename)

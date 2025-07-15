8737ea21151642a23116463ce719d936c083dfc7
brodski <cbrodski@gmail.com>
2025-07-14 20:43:48 -0600

    nothing really

--- SSScrapey-rework/terraform-ssscrapey/deploy_dev.ps1
+++ SSScrapey-rework/terraform-ssscrapey/deploy_dev.ps1
@@ -13,6 +13,6 @@
 # $ build_all_ssscrapy_dev.ps1
 $TF_ENVIRONMENT="dev"
 
-echo "Don't forget to run SSScrapey-rework/docker/build_all_ssscrapy_dev.ps1"
+echo "Don't forget to run build_all_ssscrapy_dev.ps1"
 terraform init -reconfigure -backend-config="key=${TF_ENVIRONMENT}/ssscrapey/terraform.tfstate"
 terraform apply -var-file="vars_${TF_ENVIRONMENT}.tfvars"  --auto-approve

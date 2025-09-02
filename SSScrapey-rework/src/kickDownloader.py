aeff765f4150d8bd4f0180d59b16651976b1a828
brodski <cbrodski@gmail.com>
2025-09-02 07:11:28 -0600

    removed old failed/expiremental code

--- SSScrapey-rework/src/kickDownloader.py
+++ SSScrapey-rework/src/kickDownloader.py
@@ -1,12 +1,17 @@
 import time
 import controllers.MicroDownloader.downloaderGo as downloadGo
 
+
 if __name__ == "__main__":
     print('starting go download batch...')
+
     start_time = time.time()
+
     x = downloadGo.goDownloadBatch(isDebug=False)
+
     print('Finished kickDownloader')
+
     elapsed_time = time.time() - start_time
+    print("-------------------")
     print("Total time: ", elapsed_time)
-    print("Total time: ", elapsed_time)
-    print("Total time: ", elapsed_time)
+    print("-------------------")

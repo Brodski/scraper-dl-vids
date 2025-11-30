41f28ed4715434e5c493bf4e0e6d06ffae060468
brodski <cbrodski@gmail.com>
2025-11-30 15:42:34 -0700

    tiny non-update

--- SSScrapey-rework/src/utils/ecs_meta.py
+++ SSScrapey-rework/src/utils/ecs_meta.py
@@ -3,6 +3,7 @@ import requests
 
 import logging
 from utils.logging_config import LoggerConfig
+from env_file import env_varz
 
 def logger():
     pass
@@ -10,6 +11,8 @@ def logger():
 logger: logging.Logger = LoggerConfig("micro").get_logger()
 
 def find_aws_logging_info():
+    if env_varz.ENV == "local":
+        return
     # Get metadata URI from environment variable
     metadata_uri = os.environ.get("ECS_CONTAINER_METADATA_URI_V4")
     if not metadata_uri:
@@ -41,15 +44,17 @@ def find_aws_logging_info():
         (c for c in task_metadata["Containers"] if c["Name"] == container_name), None
     )
 
-    if not container_info:
+    bski_contaier = task_metadata["Containers"][0] # the only container is my container
+
+    log_options = bski_contaier["LogOptions"]
+    awslogs_stream = log_options.get("awslogs-stream")
+    awslogs_group  = log_options.get("awslogs-group")
+    awslogs_region = log_options.get("awslogs-region")
+
+    logger.info("Stream:", awslogs_stream)
+    logger.info("Group:",  awslogs_group)
+    logger.info("Region:", awslogs_region)
+
+    if not awslogs_group:
         raise ValueError(f"Container {container_name} not found in metadata")
 
-    # Extract log info
-    log_options = container_info.get("LogOptions", {})
-    log_group = log_options.get("awslogs-group")
-    log_stream = log_options.get("awslogs-stream")
-    region = log_options.get("awslogs-region")
-
-    logger.info("Region:", region)
-    logger.info("Log Group:", log_group)
-    logger.info("Log Stream:", log_stream)
